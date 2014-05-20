037b8f3efa5b8b200b2c690e55190c183b374474
Richard H <blendblob@gmail.com>
2014-05-20 02:58:43 -0700

    A few changes

--- pointConnector.py
+++ pointConnector.py
@@ -8,7 +8,7 @@ import mathutils
 from mathutils import Vector
 def addSpherePoints(pointA,pointB):
     bpy.ops.surface.primitive_nurbs_surface_sphere_add(radius=.5, location=(pointA))
-    bpy.ops.surface.primitive_nurbs_surface_sphere_add(radius=.5, location=(pointB))
+    #bpy.ops.surface.primitive_nurbs_surface_sphere_add(radius=.2, location=(pointB))
 def addBezier(pointA,pointB):
     bpy.ops.curve.primitive_bezier_curve_add()
     obj = bpy.context.object
@@ -17,10 +17,10 @@ def addBezier(pointA,pointB):
     obj.data.bevel_depth = 0.1
     obj.data.bevel_resolution = 4
     # set first point to centre of sphere1
-    obj.data.splines[0].bezier_points[0].co = (pointA)
+    obj.data.splines[0].bezier_points[0].co = (pointA-bpy.context.scene.cursor_location)
     obj.data.splines[0].bezier_points[0].handle_left_type = 'VECTOR'
     # set second point to centre of sphere2
-    obj.data.splines[0].bezier_points[1].co = (pointB)
+    obj.data.splines[0].bezier_points[1].co = (pointB-bpy.context.scene.cursor_location)
     obj.data.splines[0].bezier_points[1].handle_left_type = 'VECTOR'
 def get_distance():
     """
@@ -67,11 +67,20 @@ def add_prism(pointA,pointB,thickness):
 #for ob in bpy.data.objects:
 #    obLoc= ob.location
 def main():
-    addSpherePoints(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location)
-    addBezier(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location)
+    print("----running ALPS")
+    for ob in bpy.context.selected_objects:
+    #for ob in bpy.data.objects:
+        #ob.select=False
+        #connect to all others
+        for ob_B in bpy.context.selected_objects:
+            print(ob.name+"----"+ob_B.name)
+            #if ob.name != ob_B.name:
+            addSpherePoints(ob.location,ob_B.location)
+            addBezier(ob.location,ob_B.location)
+            
+        #deselect
+        #new active
+    #addSpherePoints(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location)
+    #addBezier(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location)
     #add_prism(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location,0)
 main()
-
-
-if __name__ == "__main__":
-    main()
