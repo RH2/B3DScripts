4c90f9cbae39c125e412012dcc5a3c47c646281b
Richard H <blendblob@gmail.com>
2014-05-19 17:30:36 -0700

    stackoverflow copy/paste
    http://blender.stackexchange.com/questions/5898/create-a-cylinder-linking-two-points-python

--- pointConnector.py
+++ pointConnector.py
@@ -6,6 +6,22 @@ import bpy
 import math
 import mathutils 
 from mathutils import Vector
+def addSpherePoints(pointA,pointB):
+    bpy.ops.surface.primitive_nurbs_surface_sphere_add(radius=.5, location=(pointA))
+    bpy.ops.surface.primitive_nurbs_surface_sphere_add(radius=.5, location=(pointB))
+def addBezier(pointA,pointB):
+    bpy.ops.curve.primitive_bezier_curve_add()
+    obj = bpy.context.object
+    obj.data.dimensions = '3D'
+    obj.data.fill_mode = 'FULL'
+    obj.data.bevel_depth = 0.1
+    obj.data.bevel_resolution = 4
+    # set first point to centre of sphere1
+    obj.data.splines[0].bezier_points[0].co = (pointA)
+    obj.data.splines[0].bezier_points[0].handle_left_type = 'VECTOR'
+    # set second point to centre of sphere2
+    obj.data.splines[0].bezier_points[1].co = (pointB)
+    obj.data.splines[0].bezier_points[1].handle_left_type = 'VECTOR'
 def get_distance():
     """
     return: float. Distance of the two objects
@@ -51,7 +67,9 @@ def add_prism(pointA,pointB,thickness):
 #for ob in bpy.data.objects:
 #    obLoc= ob.location
 def main():
-    add_prism(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location,0)
+    addSpherePoints(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location)
+    addBezier(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location)
+    #add_prism(bpy.data.objects['Cube.001'].location,bpy.data.objects['Cube'].location,0)
 main()
 
 
