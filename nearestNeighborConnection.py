939164e5156ac09691470b001435cb26d2641e02
Richard H <blendblob@gmail.com>
2014-07-16 03:10:26 -0700

    broken /

--- nearestNeighborConnection.py
+++ nearestNeighborConnection.py
@@ -31,6 +31,17 @@ def my_handler(scene):
         #go through each object in the group
         xLength = len(pointList) #-1 
         a=0
+        childCurveIndex=0 
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.curve.primitive_bezier_curve_add()
+        obj = bpy.context.object
+        bpy.ops.object.group_link(group='dynaLines')
+        obj.material_slots.data.active_material= lineMaterial
+        obj.data.dimensions = '3D'
+        obj.data.fill_mode = 'FULL'
+        obj.data.bevel_depth = 0.1#-(1-distance/ANIM_DIST)*.08
+        obj.data.bevel_resolution = 0 #0=4points,#1=6points
+        obj.data.resolution_u=1
         for x in pointList:
             a= a+1
             if a <= xLength:
@@ -40,22 +51,14 @@ def my_handler(scene):
                     locY=pointList[indexY].location
                     distance = math.sqrt( (locX[0] - locY[0])**2 + (locX[1] - locY[1])**2 + (locX[2] - locY[2])**2)
                     if distance <= ANIM_DIST:
-                        bpy.ops.object.select_all(action='DESELECT')
-                        obj = bpy.ops.curve.primitive_bezier_curve_add()
-                        #obj = bpy.context.object
-                        obj.materiapl_slots.data.active_material= lineMaterial
-                        obj.data.dimensions = '3D'
-                        obj.data.fill_mode = 'FULL'
-                        obj.data.bevel_depth = 0.1-(1-distance/ANIM_DIST)*.08
-                        obj.data.bevel_resolution = 0 #0=4points,#1=6points
-                        obj.data.resolution_u=1
                         # set first point to centre of sphere1
-                        obj.data.splines[0].bezier_points[0].co = locX
-                        obj.data.splines[0].bezier_points[0].handle_left_type = 'VECTOR'
+                        obj.data.splines[childCurveIndex].bezier_points[0].co = locX
+                        obj.data.splines[childCurveIndex].bezier_points[0].handle_left_type = 'VECTOR'
                         # set second point to centre of sphere2
-                        obj.data.splines[0].bezier_points[1].co = locY
-                        obj.data.splines[0].bezier_points[1].handle_left_type = 'VECTOR'
-                        bpy.ops.object.group_link(group='dynaLines')                                    
+                        obj.data.splines[childCurveIndex].bezier_points[1].co = locY
+                        obj.data.splines[childCurveIndexa].bezier_points[1].handle_left_type = 'VECTOR'
+                        bpy.ops.object.group_link(group='dynaLines')
+                        childCurveIndex=childCurveIndex+1                                  
         #bpy.ops.object.select_all(action='DESELECT')
         #bpy.ops.object.select_same_group(group="dynaLines")
 class ToolsPanel(bpy.types.Panel):
